80853523c2704eee0a9613211642fca8e5d24495
Ludovic Saufnay <Ludovic Saufnay>
2017-05-08 22:10:49 +0200

    installation material-ui + dependence + implement

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+// react
 import React from 'react';
 import ReactDOM from 'react-dom';
+// material
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// internal
 import App from './App';
+// css
 import './index.css';
 
+injectTapEventPlugin();
+
+const AppWithTheme = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <App />,
+  <AppWithTheme />,
   document.getElementById('root')
 );
